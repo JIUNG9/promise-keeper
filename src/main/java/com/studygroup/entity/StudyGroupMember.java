8c48da7433c71881b13f7096f6457d315e0ce1dc
UNG <rnwldnd7248@gmail.com>
2023-05-15 13:49:43 +0900

    Feat: add the group feature

--- src/main/java/com/studygroup/entity/StudyGroupMember.java
+++ src/main/java/com/studygroup/entity/StudyGroupMember.java
@@ -1,14 +1,19 @@
 package com.studygroup.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+import com.studygroup.enums.GroupRole;
+import lombok.*;
 
 import java.util.List;
 
-@NoArgsConstructor
+
 @Entity
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Table(name ="study_group_member")
 public class StudyGroupMember extends BaseTimeEntity{
 
     @Id
@@ -16,30 +21,30 @@ public class StudyGroupMember extends BaseTimeEntity{
     private Long id;
 
     @Column(length = 256, nullable = false)
-    private String nick_name;
+    private String nickName;
 
-    @Column(columnDefinition = "boolean default false",nullable = false)
-    private Boolean isLeader;
+    @Column(length = 256, nullable = false)
+    private String intro;
+
 
     @Column(columnDefinition = "integer default 0", nullable = false)
     private int warnCount;
 
-    @Column(columnDefinition = "boolean default false", nullable = false)
-    private boolean accessPermission;
 
-    @OneToMany(cascade = {CascadeType.REMOVE, CascadeType.PERSIST}, orphanRemoval = true)
-    private List<GroupMemberSchedule> groupMemberScheduleList = new java.util.ArrayList<>();
+    @Enumerated(EnumType.STRING)
+    private GroupRole groupRole;
+
+    @OneToMany(mappedBy = "studyGroupMember",cascade = {CascadeType.REMOVE, CascadeType.PERSIST}, orphanRemoval = true)
+    private List<StudyGroupMemberPlan> studyGroupMemberPlanList;
 
     @ManyToOne
     @JoinColumn
     private StudyGroup studyGroup;
 
-    @OneToOne(mappedBy = "studyGroupMember")
-    StudyGroupMember studyGroupMember;
-
     @ManyToOne
     @JoinColumn
     private Member member;
+
 
 }
 
